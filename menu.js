5dc6cafb19857e760b4322f0be31465fc3ff6134
Ali Muqaddas <alimuqaddas92@gmail.com>
2017-10-03 22:16:27 +0500

    Initial Commit

---
+++ menu.js
@@ -0,0 +1,41 @@
+const {Menu} = require('electron')
+const electron = require('electron')
+const app = electron.app
+
+const template = [
+  {
+    role: 'window',
+    submenu: [
+      {
+        role: 'minimize'
+      },
+      {
+        role: 'close'
+      }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('http://www.cueclash.com') }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  const name = app.getName()
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        role: 'quit'
+      }
+    ]
+  })
+}
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
